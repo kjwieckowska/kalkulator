70c23b0b7f51f1f5b2052deed0796dfc45b2548e
kjwieckowska <kjwieckowska@student.wsb-nlu.edu.pl>
2023-04-25 22:18:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace kalkulator 
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double num1, num2;
+            char operation;
+
+            Console.WriteLine("Prosty kalkulator");
+            Console.WriteLine("-----------------\n");
+
+            Console.Write("Wprowadź pierwszą liczbę: ");
+            num1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Wprowadź drugą liczbę: ");
+            num2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Wybierz operację (+,-,*,/): ");
+            operation = char.Parse(Console.ReadLine());
+
+            switch (operation)
+            {
+                case '+':
+                    Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+                    break;
+                case '-':
+                    Console.WriteLine("{0} - {1} = {2}", num1, num2, num1 - num2);
+                    break;
+                case '*':
+                    Console.WriteLine("{0} * {1} = {2}", num1, num2, num1 * num2);
+                    break;
+                case '/':
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("Nie można dzielić przez zero!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Nieprawidłowa operacja!");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
